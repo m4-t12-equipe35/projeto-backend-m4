16c00d2ca4cf7ca937f240101eb611005738208d
MatheusMendesSB <matheusmendessb@gmail.com>
2022-10-31 20:26:27 -0300

    feat: appError middleware

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,11 @@ import "reflect-metadata";
 import "express-async-errors";
 import express from "express";
 
+import handleErrorMiddleware from "./middlewares/handleError.middleware";
+
 const app = express();
 app.use(express.json());
+
+app.use(handleErrorMiddleware);
 
 export default app;
