f8fe5cd77fd43c85f7ad60f393be131a57945e74
MatheusMendesSB <matheusmendessb@gmail.com>
2022-10-31 19:31:06 -0300

    feat: data-source, app & server

---
+++ src/data-source.ts
@@ -0,0 +1,17 @@
+import { DataSource } from "typeorm";
+import "dotenv/config";
+
+const AppDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.HOST,
+  port: 5432,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  synchronize: false,
+  logging: true,
+  entities: ["src/entities/*.ts"],
+  migrations: ["src/migrations/*.ts"],
+});
+
+export default AppDataSource;
