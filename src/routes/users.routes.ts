3cc46e77c1a01d4f2f439f94b6563d66c57d95f9
MatheusMendesSB <matheusmendessb@gmail.com>
2022-11-02 11:04:20 -0300

    fix: listUsersById renamed to retrieveUser

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,12 +2,12 @@ import { Router } from "express";
 
 import createUserController from "../controllers/users/createUser.controller";
 import listUsersController from "../controllers/users/listUsers.controller";
-import listUsersByIdController from "../controllers/users/listUsersById.controller";
+import retrieveUserController from "../controllers/users/retrieveUser.controller";
 
 const usersRoutes = Router();
 
 usersRoutes.post("", createUserController);
 usersRoutes.get("", listUsersController);
-usersRoutes.get("/:id", listUsersByIdController);
+usersRoutes.get("/:id", retrieveUserController);
 
 export default usersRoutes;
