ac95c2608c77d8a67511f218074957422c624927
MatheusMendesSB <matheusmendessb@gmail.com>
2022-11-02 15:39:38 -0300

    fix: updateUser controller

--- src/controllers/users/updateUser.controller.ts
+++ src/controllers/users/updateUser.controller.ts
@@ -1,26 +1,19 @@
 import { Request, Response } from "express";
+import { instanceToPlain } from "class-transformer";
 import { IUserUpdate } from "../../interfaces/users";
 import { User } from "../../entities/user.entity";
 import updateUserService from "../../services/users/updateUser.service";
 
 const updateUserController = async (req: Request, res: Response) => {
-  try {
-    const user: IUserUpdate = req.body;
-    const id: string = req.params.id;
-    const updatedUser = await updateUserService(user, id);
-    if (updatedUser instanceof User) {
-      return res.json(updatedUser);
-    }
-    return res.status(updatedUser[1] as number).json({
-      message: updatedUser[0],
-    });
-  } catch (error) {
-    if (error instanceof Error) {
-      return res.status(404).json({
-        message: error.message,
-      });
-    }
+  const user: IUserUpdate = req.body;
+  const id: string = req.params.id;
+  const updatedUser = await updateUserService(user, id);
+  if (updatedUser instanceof User) {
+    return res.json(instanceToPlain(updatedUser));
   }
+  return res.status(updatedUser[1] as number).json({
+    message: updatedUser[0],
+  });
 };
 
 export default updateUserController;
