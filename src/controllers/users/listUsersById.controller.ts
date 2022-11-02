475e2125a4d665506aea9f4367d85c73aa38b535
helena <heju1958@outlook.com>
2022-11-02 08:50:39 -0300

    feat: controller user by id type

--- src/controllers/users/listUsersById.controller.ts
+++ src/controllers/users/listUsersById.controller.ts
@@ -3,7 +3,7 @@ import { instanceToPlain } from "class-transformer";
 import listUsersByIdService from "../../services/users/listUsersById.service";
 
 const listUsersByIdController = async (req: Request, res: Response) => {
-  const id = req.params.id;
+  const id: string = req.params.id;
   const usersById = await listUsersByIdService(id);
   return res.json(instanceToPlain(usersById));
 };
