2b8da2dd9f82810e6eadf3182a8c98c2f58a5e59
helena <heju1958@outlook.com>
2022-11-02 08:51:48 -0300

    feat: route list user by id

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import createUserController from "../controllers/users/createUser.controller";
 import listUsersController from "../controllers/users/listUsers.controller";
+import listUsersByIdController from "../controllers/users/listUsersById.controller";
 
 const usersRoutes = Router();
 
 usersRoutes.post("", createUserController);
 usersRoutes.get("", listUsersController);
+usersRoutes.get("/:id", listUsersByIdController);
 
 export default usersRoutes;
