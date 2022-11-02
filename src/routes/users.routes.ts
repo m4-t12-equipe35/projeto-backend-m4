9bcd6d9a9e6c546d2ebcfbd9fb9488aae15110da
helena <heju1958@outlook.com>
2022-11-02 12:02:58 -0300

    feat: update user by id

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -4,12 +4,14 @@ import createUserController from "../controllers/users/createUser.controller";
 import deleteUserController from "../controllers/users/deleteUser.controller";
 import listUsersController from "../controllers/users/listUsers.controller";
 import retrieveUserController from "../controllers/users/retrieveUser.controller";
+import updateUserController from "../controllers/users/updateUser.controller";
 
 const usersRoutes = Router();
 
 usersRoutes.post("", createUserController);
 usersRoutes.get("", listUsersController);
 usersRoutes.get("/:id", retrieveUserController);
+usersRoutes.patch("/:id", updateUserController);
 usersRoutes.delete("/:id", deleteUserController);
 
 export default usersRoutes;
