cf7a6a2438fbe9170b3ffd85dcabcd7e7dd60dd0
MatheusMendesSB <matheusmendessb@gmail.com>
2022-11-03 22:19:49 -0300

    fix: techs routes push error

--- src/routes/techs.routes.ts
+++ src/routes/techs.routes.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 
-import createTechController from "../controllers/Techs/createTech.controller";
-import listTechsController from "../controllers/Techs/listTechs.controller";
-import deleteQuestionController from "../controllers/questions/deleteQuestion.controller";
+import createTechController from "../controllers/techs/createTech.controller";
+import listTechsController from "../controllers/techs/listTechs.controller";
+import deleteTechController from "../controllers/techs/deleteTech.controller";
 
 const techsRoutes = Router();
 
 techsRoutes.post("/", createTechController);
 techsRoutes.get("/", listTechsController);
-techsRoutes.delete("/:id", deleteQuestionController);
+techsRoutes.delete("/:id", deleteTechController);
 
 export default techsRoutes;
